c41ab6a5f36782e4f21fb1bf4f3b1362f9b02068
paraguaia-maria <162311915+paraguaia-maria@users.noreply.github.com>
2024-07-30 08:16:12 -0300

    Add files via upload

---
+++ 2tri-av1-main/2tri-av1-main/2tri-av1-main/mec/main.js
@@ -0,0 +1,37 @@
+var cxPrincipal = document. querySelector ("cx-principal");
+var cxPerguntas = document. querySelector ("cx-perguntas")
+var cxAlternativas = document. querySelector ("cx-lternativa")
+var cxResulado = document. querySelector ("cx-resultado")
+var cxResultado = document. querySelector ("cx-resultado")
+var peguntas=[
+{
+    enunciado: "A IA pode automatizar tarefas repetitivas.",
+    alternativa:[
+        "Vai eliminar tarefas monotonas", "Vai tirar muitos empregos"
+    ]
+},
+{
+    enunciado: "",
+    alternativa:[
+        "", ""
+    ]
+},
+{
+    enunciado: "",
+    alternativa:[
+        "", ""
+    ]
+},
+{
+    enunciado: "",
+    alternativa:[
+        "", ""
+    ]
+},
+{
+    enunciado: "",
+    alternativa:[
+        "", ""
+    ]
+}
+]
